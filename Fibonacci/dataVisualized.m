8bf0fed6e2403460fae8febe1add7b7ca0e7db36
talhakhalil0703 <tkhalil0703@gmail.com>
2020-02-05 22:59:31 -0700

    Figures created but might rerun data

--- Fibonacci/dataVisualized.m
+++ Fibonacci/dataVisualized.m
@@ -1,5 +1,8 @@
-data = readmatrix("matrix.csv")
-filtered = medfilt1(data,3);
+data = readmatrix("iterator.csv");
+filtered = medfilt1(data,6);
+xdata = filtered(:,1)
+ydata = filtered(:,2)
+
 x = max(filtered);
 
 plot (filtered(:,end))
