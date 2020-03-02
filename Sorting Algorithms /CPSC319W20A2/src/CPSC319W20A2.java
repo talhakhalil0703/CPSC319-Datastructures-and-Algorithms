828cc564aff00fbc3c80c5e02c83a43329e84bdb
talhakhalil0703 <tkhalil0703@gmail.com>
2020-03-02 00:00:39 -0700

    Quick sorted all the words in the list by alphabet

---
+++ Sorting Algorithms /CPSC319W20A2/src/CPSC319W20A2.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CPSC319W20A2 {
+
+    ArrayList<String> words = new ArrayList<String>();
+
+    public void readWords(String fileName){
+
+        try {
+            String fileLocation = System.getProperty("user.dir") + "/" +fileName;
+            System.out.println(fileLocation);
+            FileReader fileToRead = new FileReader(fileLocation);
+            BufferedReader buffer =  new BufferedReader(fileToRead);
+
+            String line = "";
+
+            while((line = buffer.readLine()) != null){
+                String word = line;
+                this.words.add(word);
+            }
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void printReadWords(int start, int end){
+        if (this.words.size() <= 0){
+            System.out.println("No words have been read as of yet.");
+            return;
+        }
+        for (int i = start; i < end; i++){
+            String wordToPrint = this.words.get(i);
+            System.out.println(wordToPrint);
+        }
+    }
+
+     public void quickSortWordsAlphabetically(int start, int end){
+        //Average running time is nlogn, and worst case is n^2 . However it is in place sorting which allows us to not consume a lot of memory especially when dealing with large data set
+         if(start >= end)
+             return;
+        int pivotIndex = partitionSort(start, end);
+        quickSortWordsAlphabetically(start, pivotIndex-1);
+        quickSortWordsAlphabetically(pivotIndex+1, end);
+    }
+
+    private int partitionSort(int start, int end){
+        String pivot = this.words.get(end);
+        String temp;
+        int pivotIndex = start;
+
+        for (int i = start; i <= (end-1); i++){
+            if (isALessThanB(this.words.get(i), pivot)){
+                swapValuesAtIndex(i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+       swapValuesAtIndex(pivotIndex, end);
+        return pivotIndex;
+    }
+
+    private void swapValuesAtIndex(int a, int b){
+        String temp = this.words.get(a);
+        this.words.set(a, this.words.get(b));
+        this.words.set(b, temp);
+    }
+
+    private boolean isALessThanB(String A, String B){
+        int shortestLength = A.length();
+
+        if (A.length() > B.length())
+           shortestLength = B.length();
+
+        char [] Achar = A.toCharArray();
+        char [] Bchar = B.toCharArray();
+
+        for (int i = 0; i < shortestLength; i++) {
+            if (Achar[i] < Bchar[i])
+                return true;
+             else if (Achar[i] > Bchar[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String [] args){
+        CPSC319W20A2 anagram = new CPSC319W20A2();
+
+        for(int i = 0; i < args.length; i++) {
+            System.out.println(i + " "+ args[i]);
+        }
+        anagram.readWords(args[0]);
+        anagram.printReadWords(0, anagram.words.size());
+        anagram.quickSortWordsAlphabetically(0, (anagram.words.size()-1));
+        System.out.println("Sorted words below:");
+        anagram.printReadWords(0, anagram.words.size());
+    }
+
+
+}
