701356ffda38c3eb49c7d6b8b8106b128bba51a9
talhakhalil0703 <tkhalil0703@gmail.com>
2020-03-02 22:38:54 -0700

    Restructuring

---
+++ Sorting Algorithms /CPSC319W20A2/src/FileManager.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class FileManager {
+
+    public ArrayList<String> readWords(String fileName){
+
+        try {
+            String fileLocation = System.getProperty("user.dir") + "/" +fileName;
+            System.out.println(fileLocation);
+            FileReader fileToRead = new FileReader(fileLocation);
+            BufferedReader buffer =  new BufferedReader(fileToRead);
+
+            String line = "";
+
+            while((line = buffer.readLine()) != null){
+                String word = line;
+                this.words.add(word);
+            }
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
