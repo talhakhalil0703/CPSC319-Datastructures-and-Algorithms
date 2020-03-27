5e372786e23a7c96bc31c3a54fbea1abb5818cb6
talhakhalil0703 <tkhalil0703@gmail.com>
2020-03-27 05:41:24 -0600

    Code is done

--- CPSC319W20A3/src/CPSC319W20A3.java
+++ CPSC319W20A3/src/CPSC319W20A3.java
@@ -1,9 +1,13 @@
+/**
+ * Assignment 3, this file run the code of te Binary Tree and gets the inital user input
+ *
+ * @author Talha Khalil
+ * @since March 26, 2020
+ */
 public class CPSC319W20A3 {
     public static void main(String [] args ){
-        CPSC319W20A3 cpsc = new CPSC319W20A3();
         InputManager input = new InputManager();
         String fileName = input.promptUserForFile();
         BinaryTree BT = new BinaryTree(input.getWords(),fileName);
     }
-
 }
